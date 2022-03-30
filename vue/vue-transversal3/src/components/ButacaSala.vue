0ac9216e057b66fee07ff1bdcb99658fa65ef3c9
AVictorMoreno <a20vicmormor@inspedralbes.cat>
2022-03-30 13:16:04 +0200

    componente cardpeli dia y homeview

---
+++ vue/vue-transversal3/src/components/ButacaSala.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h2>Insertar imagen de butaca</h2>
+  </div>
+</template>
--- vue/vue-transversal3/src/components/CardPeliDia.vue
+++ vue/vue-transversal3/src/components/CardPeliDia.vue
@@ -4,9 +4,28 @@ import { RouterLink, RouterView } from "vue-router";
 <template>
   <div>
     <h2>ESTO ES LA PELI DEL DIA</h2>
+    <div class="card" style="width: 18rem">
+      <img
+        class="card-img-top"
+        :src="this.infoPelicula.peli.imgPeli"
+        alt="Card image cap"
+      />
+      <div class="card-body">
+        <h5 class="card-title">{{ infoPelicula.peli.nombrePeli }}</h5>
+      </div>
+    </div>
+    <div>
+      <h5>{{ infoPelicula.fecha }}</h5>
+      <h5>{{ infoPelicula.hora }}</h5>
+    </div>
     <RouterLink class="btn btn-primary" to="/seleccionarButacas"
       >Comprar Entrades</RouterLink
     >
     <RouterView />
   </div>
 </template>
+<script>
+export default {
+  props: ["infoPelicula"],
+};
+</script>
