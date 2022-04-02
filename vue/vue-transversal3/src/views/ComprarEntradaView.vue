e45381634c4019e4a3b4097be8697b034b306a9b
a20paumunoli <a20paumunoli@inspedralbes.cat>
2022-04-03 00:06:33 +0200

    Update ComprarEntradaView.vue
    Cambios

--- vue/vue-transversal3/src/views/ComprarEntradaView.vue
+++ vue/vue-transversal3/src/views/ComprarEntradaView.vue
@@ -1,19 +1,3 @@
-
-<template>
-  <div>
-    <q-btn flat class="btn btn-secondary" label="Volver" @click="retroceder()"
-      >Volver</q-btn
-    >
-
-    <h1>COMPRAR ENTRADES DE LA SESION {{ $route.params.idSesion }}</h1>
-    <CardPeliGeneral :peliInfo="peliSeleccionada" />
-    <SeleccionarButaques />
-    <RouterLink class="btn btn-primary" to="/pagament"
-      >Comprar Entrades</RouterLink
-    >
-    <RouterView />
-  </div>
-</template>
 <script>
 import { RouterLink, RouterView } from "vue-router";
 import CardPeliGeneral from "@/components/CardPeliGeneral.vue";
@@ -24,20 +8,23 @@ export default {
   computed: {
     ...mapStores(sessioStore),
   },
+  
   data() {
     return {
       peliSeleccionada: Object,
     };
   },
+
   components: {
     CardPeliGeneral,
     SeleccionarButaques,
   },
+
   beforeCreate() {
     console.log(window.location.href.split("/", 5)[4]);
     fetch(
       "http://192.168.1.145:8000/sesionEspecifica?idSesion=" +
-        window.location.href.split("/", 5)[4]
+      window.location.href.split("/", 5)[4]
     )
       .then((response) => response.json())
       .then((data) => {
@@ -47,6 +34,7 @@ export default {
         console.log(this.peliSeleccionada);
       });
   },
+
   methods: {
     retroceder() {
       window.history.back();
@@ -54,5 +42,17 @@ export default {
   },
 };
 </script>
+
+<template>
+  <div>
+    <q-btn flat class="btn btn-secondary" label="Volver" @click="retroceder()">Volver</q-btn>
+
+    <h1>COMPRAR ENTRADES DE LA SESION {{ $route.params.idSesion }}</h1>
+    <CardPeliGeneral :peliInfo="peliSeleccionada" />
+    <SeleccionarButaques />
+    <RouterLink class="btn btn-primary" to="/pagament">Comprar Entrades</RouterLink>
+    <RouterView />
+  </div>
+</template>
 
 <style></style>
