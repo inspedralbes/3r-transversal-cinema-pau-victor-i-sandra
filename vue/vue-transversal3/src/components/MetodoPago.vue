855a6ed0555fbdf7d43620d552cdf1a6124f6b55
pauchinpun <a20paumunoli@inspedralbes.cat>
2022-04-04 09:13:36 +0200

    Añadir fetch para enviar las entradas compradas

--- vue/vue-transversal3/src/components/MetodoPago.vue
+++ vue/vue-transversal3/src/components/MetodoPago.vue
@@ -1,3 +1,82 @@
+<script>
+import { sessioStore } from "../stores/sessioStore";
+import { mapStores } from "pinia";
+export default {
+  computed: {
+    ...mapStores(sessioStore),
+  },
+
+  data() {
+    return {
+      datosPinia: null
+    }
+  },
+
+  mounted() {
+    this.datosPinia = this.sessioStore.get;
+  },
+
+
+  methods: {
+    comprarEntradas: function () {
+      let entradas = new FormData();
+      let butacasReservadas = {};
+      entradas.append('idUsuari', 1);
+      entradas.append('idSesion', this.datosPinia.idSesion);
+      this.datosPinia.butacasSeleccionadas.forEach((butaca) => {
+        butacasReservadas[butaca] = this.precioEntradas(butaca)
+      });
+
+      entradas.append('butacasReservadas', JSON.stringify(butacasReservadas))
+
+      fetch(
+        "http://192.168.210.161:8000/entradas", {
+        method: "POST",
+        body: entradas
+      })
+        .then((response) => response.json())
+        .then((data) => {
+         console.log(data);
+        });
+    },
+
+    precioEntradas: function (butaca) {
+      let precio = 0;
+
+      if (this.datosPinia.diaEspectador) {
+        if (this.datosPinia.vip) {
+          if (butaca >= 'b51' && butaca <= 'b60') { // b51 - b60 => butacas entre las que se encuntran las VIP (ambos incluidos)
+            precio += 6;
+            console.log()
+          } else {
+            precio += 4;
+          }
+        } else {
+          precio += 4;
+        }
+      } else {
+        if (this.datosPinia.vip) {
+          if (butaca >= 'b51' && butaca <= 'b60') { // b51 - b60 => butacas entre las que se encuntran las VIP (ambos incluidos)
+            precio += 8;
+          } else {
+            precio += 6;
+          }
+        } else {
+          precio += 6;
+        }
+      }
+
+      return precio;
+    }
+
+
+
+  },
+}
+</script>
+
+
+
 <template>
   <h2>Pagar</h2>
   <form class="row g-3">
@@ -24,15 +103,19 @@
       </div>
     </div>
     <div class="col-12">
-      <RouterLink class="btn btn-primary" to="/realitzatpagament">Pagar</RouterLink>
+      <RouterLink
+        class="btn btn-primary"
+        @click.native="this.comprarEntradas"
+        to="/realitzatpagament"
+      >Pagar</RouterLink>
       <RouterView />
-      <a href="https://www.paypal.com/es/home" class="btn btn-danger paypal">PayPal</a>
+      <a @click="this.comprarEntradas" class="btn btn-danger paypal">PayPal</a>
     </div>
   </form>
 </template>
 
 <style>
-.paypal{
+.paypal {
   margin-left: 10px;
 }
 </style>
--- vue/vue-transversal3/src/views/ComprarEntradaView.vue
+++ vue/vue-transversal3/src/views/ComprarEntradaView.vue
@@ -1,21 +1,3 @@
-<<<<<<< Updated upstream
-=======
-<template>
-  <div>
-    <q-btn flat class="btn btn-secondary" label="Volver" @click="retroceder()"
-      >Volver</q-btn
-    >
-
-    <h1>COMPRAR ENTRADES DE LA SESION {{ $route.params.idSesion }}</h1>
-    <CardPeliGeneral :peliInfo="peliSeleccionada" />
-    <SeleccionarButaques />
-    <RouterLink class="btn btn-primary" to="/pagament"
-      >Comprar Entrades</RouterLink
-    >
-    <RouterView />
-  </div>
-</template>
->>>>>>> Stashed changes
 <script>
 import CardPeliGeneral from "@/components/CardPeliGeneral.vue";
 import SeleccionarButaques from "@/components/SeleccionarButaques.vue";
@@ -40,7 +22,7 @@ export default {
   beforeCreate() {
     console.log(window.location.href.split("/", 5)[4]);
     fetch(
-      "http://192.168.1.145:8000/sesionEspecifica?idSesion=" +
+      "http://192.168.210.161:8000/sesionEspecifica?idSesion=" +
       window.location.href.split("/", 5)[4]
     )
       .then((response) => response.json())
