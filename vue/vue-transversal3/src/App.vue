50c6fa0c3abbc2fdf8bc90756b29165a732f01d6
ASandraOrtiz <a20sanortveg@inspedralbes.cat>
2022-04-05 14:09:43 +0200

    modal consultar entradas

--- vue/vue-transversal3/src/App.vue
+++ vue/vue-transversal3/src/App.vue
@@ -5,6 +5,18 @@ export default {
     RouterLink,
     RouterView,
   },
+  
+  data() {
+    return {
+      mostrarFormUsr: true,
+    }
+  },
+
+  methods: {
+    consultarEntradas: function(){
+      this.mostrarFormUsr=false;
+    }
+  }
 };
 </script>
 
@@ -24,7 +36,7 @@ export default {
             <a class="nav-link link" aria-current="page" data-bs-toggle="modal" data-bs-target="#exampleModal">Consultar entradas</a>
 
             <!-- MODAL -->
-            <div class="modal fade " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal fade " id="exampleModal" data-bs-backdrop="static" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
               <div class="modal-dialog modal-dialog-centered  modal-lg">
                 <div class="modal-content">
                   <div class="modal-header">
@@ -32,6 +44,9 @@ export default {
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body align-self-center grande">
+
+                    <!-- From usuario -->
+                    <div id="form_usr" :class="{ocultar : !mostrarFormUsr}">
                      <div class="col ">
                         <div class="col">
                           <label for="email" class="form-label text-left">Email</label>
@@ -44,10 +59,21 @@ export default {
                         </div>
                         <br>
                         <div class="col text-center">
-                          <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Aceptar</button>
+                          <button type="button" @click="consultarEntradas" class="btn btn-primary">Aceptar</button>
                         </div>
                       </div>
+                    </div>
                   </div>
+                  <!-- Fin Form usuario -->
+
+                  <!-- Entradas  -->
+                  <div id="entradas" :class="{ocultar : mostrarFormUsr}">
+                    <p>Poner grid con las entradas</p>
+                  </div>
+                  
+                  
+                  <!-- Fin entradas  -->
+
                   <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
                   </div>
@@ -69,48 +95,55 @@ export default {
   <RouterView/>
 </template>
 
+
+
 <style>
-.header {
-  background-color: black !important;
-  color: white;
-}
+  .header {
+    background-color: black !important;
+    color: white;
+  }
 
-.logo {
-  width: 180px;
-}
+  .logo {
+    width: 180px;
+  }
 
-.grande {
-  width: 290px;
-}
+  .grande {
+    width: 290px;
+  }
 
-.nav-link {
-  color: #13afed;
-  font-weight: 550;
-  font-size: 16px;
-}
+  .nav-link {
+    color: #13afed;
+    font-weight: 550;
+    font-size: 16px;
+  }
 
-.nav-link:hover {
-  color: #fdcb31;
-}
+  .nav-link:hover {
+    color: #fdcb31;
+  }
 
-a {
-  text-decoration: none;
-}
+  a {
+    text-decoration: none;
+  }
 
-.vr {
-  border-left: 2px solid #4dcdff;
-  position: absolute;
-  left: 67%;
-  height: 20px;
-  top: 10px;
-}
+  .vr {
+    border-left: 2px solid #4dcdff;
+    position: absolute;
+    left: 67%;
+    height: 20px;
+    top: 10px;
+  }
 
-body {
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-}
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
 
-.modal {
-  color: black;
-}
+  .modal {
+    color: black;
+  }
+
+  .ocultar {
+    display: none;
+  }
+
 </style>
