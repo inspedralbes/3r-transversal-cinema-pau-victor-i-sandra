397e46d80978da791e5ca17cdc3fd77371e5793e
AVictorMoreno <92580351+AVictorMoreno@users.noreply.github.com>
2022-04-18 00:43:22 +0200

    boton modal

--- vue/vue-transversal3/src/components/Header.vue
+++ vue/vue-transversal3/src/components/Header.vue
@@ -104,7 +104,7 @@
                   </div>
                   <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-                      Close
+                      Cerrar
                     </button>
                   </div>
                 </div>
